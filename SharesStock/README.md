50c3a74986857d6075e12ed7ba15c5728286f8b4
jlegendary <j.legendary2@gmail.com>
2015-08-03 18:30:33 -0400

    Added stock problem program

---
+++ SharesStock/README.md
@@ -0,0 +1,9 @@
+Input:
+	Number of Stocks to buy
+	Purchase price
+	Selling price
+
+Outputs:
+	Amount invested
+	Service charged
+	Final profit
---
+++ SharesStock/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//
+//  Created by Lejendary on 8/3/15.
+//  Copyright (c) 2015 Lejendary. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    // variables
+const double servCharge = 0.015;
+int numShareSold;
+double buyShare, sellShare, amountInvested, totServCharge, netAmount;
+
+int main ()
+{   //inputs
+    cout << "How many shares did you sell? " << endl;
+    cin >> numShareSold;
+    cout << "\nWhat was the purchase price of each share? \n$";
+    cin >> buyShare;
+    cout << "\nWhat was the selling price of each share? \n$";
+    cin >> sellShare;
+    
+    //formulas
+    amountInvested = numShareSold * buyShare;
+    totServCharge = (numShareSold * sellShare) * servCharge;
+    netAmount = (numShareSold * sellShare) - totServCharge;
+    
+    //decimals
+    cout << fixed << showpoint << setprecision(2);
+    
+    //outputs
+    cout << "Amount invested: $" << amountInvested << endl;
+    cout << "Service charged: $" << totServCharge << endl;
+    cout << "Final profit/loss: $" << netAmount - amountInvested << endl;
+    
+    return 0;
+}
