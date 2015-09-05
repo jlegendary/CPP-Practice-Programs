4db9d4573822cfffc761580717fa71da4e1c1057
jlegendary <j.legendary2@gmail.com>
2015-09-05 16:33:27 -0400

    Added headerfile

---
+++ Football Record/footballData.h
@@ -0,0 +1,33 @@
+//
+//  footballData.h
+//   Headerfile
+//
+//  Created by JLegendary on 9/5/15.
+//  Copyright (c) 2015 JLegendary. All rights reserved.
+//   JLegendary@me.com
+//
+
+
+#ifndef _footballData_
+#define _footballData_
+
+#include <stdio.h>
+
+
+//public class
+class FootballData
+{
+public:
+    void readToArray();
+    void footballMenu();
+    void selectPlayers();
+    void viewPlayers();
+    void displayPlayers();
+    void updatePlayers();
+    void quitProgram();
+    
+    
+};
+
+
+#endif /* defined(_footballData_) */
